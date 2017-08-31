0c674cbd69673b0fe41d1cdddece5b721aa860af
ejerson <ejersonb@gmail.com>
2017-08-31 15:45:05 -0600

    New project files, with file implementing "Hell World Program

---
+++ src/edu/cnm/deepdive/helloworld/HelloWorld.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package edu.cnm.deepdive.helloworld;
+
+/**
+ * @author edge26
+ * 
+ * 
+ * simple class that displays a "Hello World!" message
+ */
+public class HelloWorld {
+  
+  /**
+   * prints hello world message
+   * @param args not being used
+   */
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+    
+
+  }
+
+}
